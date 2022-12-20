6131d9ab2aa1a9ea2870338ba024ad583671cb69
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2022-12-20 16:32:35 -0300

    Initial Commit

---
+++ TransactionalKeyValueStore/TransactionalKeyValueStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionalKeyValueStoreApp.swift
+//  TransactionalKeyValueStore
+//
+//  Created by Jonathan Souza on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TransactionalKeyValueStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
